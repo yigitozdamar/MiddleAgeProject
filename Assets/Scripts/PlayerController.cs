1e69deda137a57d01e3267bd7b7c9a89ad25b351
yigitozdamar <yigitozdamar@hotmail.com>
2021-10-15 00:03:48 +0300

    V1.2

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,27 +5,35 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Animator playerAnim;
-    private Rigidbody playerRb;
-    private bool isWalk = false;
-    private float horizontalInput;
-    private float verticalInput;
-    public float walkSpeed = 2f;
-    public float rotationSpeed = 200f;
+    public CharacterController controller;
+    public float speed = 4f;
 
     void Awake()
     {
         playerAnim = GetComponent<Animator>();
-        playerRb = GetComponent<Rigidbody>();
+        
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        horizontalInput = Input.GetAxis("Horizontal");
-        verticalInput = Input.GetAxis("Vertical");
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        float verticalInput = Input.GetAxisRaw("Vertical");
+
+        Vector3 direction = new Vector3(verticalInput, 0, -horizontalInput).normalized;
+
+        if (direction.magnitude >=0.1f)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.y) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(0f, targetAngle, 0);
+            controller.Move(direction * speed);
+            playerAnim.SetBool("isWalk", true);
+        }
+        else
+        {
+            playerAnim.SetBool("isWalk", false);
+        }
         
-        playerRb.AddForce(Vector3.forward * walkSpeed * verticalInput);
-        transform.Rotate(Vector3.up, rotationSpeed * horizontalInput);
 
 
     }
--- Assets/Scripts/VillagersPatrollingScript.cs
+++ Assets/Scripts/VillagersPatrollingScript.cs
@@ -9,20 +9,27 @@ public class VillagersPatrollingScript : MonoBehaviour
     public PathType pathSystem = PathType.Linear;
     public Vector3[] points = new Vector3[2];
     public Tween t;
+    Animator animator;
+    //Sequence mySeq = DOTween.Sequence();
 
     // Start is called before the first frame update
     void Start()
     {
-        
         t = male_1.transform.DOPath(points, 15, pathSystem);
         t.SetEase(Ease.Linear).SetLoops(-1);
+
+        animator = GetComponent<Animator>();
+        animator.SetBool("isWalk", true);
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        
+        //mySeq.Append(transform.DOMoveX(13, 5));
+        //mySeq.Append(transform.DORotate(new Vector3(0, 270, 0), 1));
+        //mySeq.Append(transform.DOMoveX(-8, 5));
 
-        
-        
     }
 }
